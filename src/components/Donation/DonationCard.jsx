9877aff4d4afa56fbe5c96dc1aed1fb0a5d83007
NF RIDOY <nfridoy@gmail.com>
2023-09-25 14:05:49 +0600

    fix: json data color in card

--- src/components/Donation/DonationCard.jsx
+++ src/components/Donation/DonationCard.jsx
@@ -3,19 +3,34 @@
 export default function DonationCard({ donation }) {
     const { id, picture, title, category, category_bg, card_bg, text_button_bg, description, price } = donation;
 
+    const cardBg = {
+        backgroundColor: card_bg
+    }
+
+    const categoryColor = {
+        backgroundColor: category_bg,
+        color: text_button_bg
+    }
+
+    const textButtonBg = {
+        color: text_button_bg
+    }
     
     return (
         <div>
-            <div className={`bg-[${card_bg}] w-fit`}>
+            {/* <div className={`bg-[${card_bg}] w-fit`}> */}
+            <div className={` w-fit`} style={cardBg}>
                 <div className="">
                     <img src={picture} alt="" />
                 </div>
                 <div>
-                    <h3 className={`bg-[${category_bg}] text-[${text_button_bg}]`}>
+                    {/* <h3 className={`bg-[${category_bg}] text-[${text_button_bg}]`}> */}
+                    <h3 style={categoryColor}>
                         {category}
                         {/* {category_bg+''} */}
                     </h3>
-                    <h2 className={`text-[${text_button_bg}]`}>
+                    {/* <h2 className={`text-[${text_button_bg}]`}> */}
+                    <h2 style={textButtonBg}>
                         {title}
                     </h2>
                 </div>
