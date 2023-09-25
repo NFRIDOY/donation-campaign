5c9e3a5e896b9757591ed7bbc2d039ce4d03d5ac
NF RIDOY <nfridoy@gmail.com>
2023-09-25 17:25:41 +0600

    Data json file image updated + daisyui card add to DonationCard

--- src/components/Donation/DonationCard.jsx
+++ src/components/Donation/DonationCard.jsx
@@ -15,26 +15,18 @@ export default function DonationCard({ donation }) {
     const textButtonBg = {
         color: text_button_bg
     }
-    
+
     return (
         <div>
-            {/* <div className={`bg-[${card_bg}] w-fit`}> */}
-            <div className="w-[500]" style={cardBg}>
-                <div className="">
-                    <img className="object-cover" src={picture} alt="" />
-                </div>
-                <div className="p-5">
-                    {/* <h3 className={`bg-[${category_bg}] text-[${text_button_bg}]`}> */}
-                    <h3 style={categoryColor} className="w-fit p-2 text-sm rounded-lg">
-                        {category}
-                        {/* {category_bg+''} */}
-                    </h3>
-                    {/* <h2 className={`text-[${text_button_bg}]`}> */}
-                    <h2 style={textButtonBg} className="text-2xl">
-                        {title}
-                    </h2>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl" style={cardBg}>
+                <figure><img src={picture} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 style={categoryColor} className="w-fit p-2 text-sm rounded-lg">{category}</h2>
+                    <p className="card-title"  style={textButtonBg}>{title}</p>
+                    
                 </div>
             </div>
+            
         </div>
     )
 }
@@ -42,3 +34,21 @@ export default function DonationCard({ donation }) {
 // DonationCard.prototype = {
 //     donation.ProtoType =
 // }
+
+// {/* <div className={`bg-[${card_bg}] w-fit`}> */}
+// <div className="" style={cardBg}>
+// <div className="">
+//     <img className="object-cover" src={picture} alt="" />
+// </div>
+// <div className="p-5">
+//     {/* <h3 className={`bg-[${category_bg}] text-[${text_button_bg}]`}> */}
+//     <h3 style={categoryColor} className="w-fit p-2 text-sm rounded-lg">
+//         {category}
+//         {/* {category_bg+''} */}
+//     </h3>
+//     {/* <h2 className={`text-[${text_button_bg}]`}> */}
+//     <h2 style={textButtonBg} className="text-2xl">
+//         {title}
+//     </h2>
+// </div>
+// </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
   console.log(donations);
 
   return (
-    <div className="border-red-400 border-2 flex justify-center">
-      <div className="border-2 border-blue-400">
+    <div className=" flex justify-center">
+      <div className="">
         <div className="grid grid-cols-3 gap-x-9 gap-y-9">
           {
             donations?.map((donation) => <DonationCard key={donation.id} donation={donation}></DonationCard>)
