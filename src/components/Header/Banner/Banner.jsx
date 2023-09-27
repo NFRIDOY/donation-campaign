4fd8ce4b1b7c499c6b10d1b688bb871f41c6e723
NF RIDOY <nfridoy@gmail.com>
2023-09-27 23:03:19 +0600

    search form done

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,6 +1,13 @@
 
 
-export default function Banner() {
+export default function Banner({ setSearch }) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const searchValue = e.target[0].value;
+
+        console.log(searchValue);
+        setSearch(searchValue);
+    }
     return (
         <div>
             <div className="hero min-h-[50vh] mb-14 " style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
@@ -9,10 +16,12 @@ export default function Banner() {
                     <div className="max-w-screen-xl">
                         <h1 className="mb-5 text-5xl font-bold text-black">I Grow By Helping People In Need</h1>
 
-                        <div className="flex justify-center">
-                            <div className="form-control w-72 flex flex-row">
-                                <input type="text" placeholder="Search" className="input input-bordered w-full md:w-auto rounded-l-lg" />
-                                <button className="btn bg-[#FF444A] text-white hover:bg-black hover:text-white">Search</button>
+                        <div className="flex justify-center gap-2">
+                            <div >
+                                <form action="" onSubmit={handleSubmit} className="form-control w-72 flex flex-row gap-1">
+                                    <input type="text" placeholder="Search By Category" className="input input-bordered w-full md:w-auto rounded-l-lg text-black font-bold" />
+                                    <button type="submit" onClick={() => setSearch()} className="btn bg-[#FF444A] text-white hover:bg-black hover:text-white">Search</button>
+                                </form>
                             </div>
                         </div>
                     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,27 @@
 import { useLoaderData } from "react-router-dom"
 import DonationCard from "../../components/Donation/DonationCard";
 import Banner from "../../components/Header/Banner/Banner";
+import { useState } from "react";
 
 
 export default function Home() {
 
   const donations = useLoaderData();
   // console.log(donations);
+  const [search, setSearch] = useState("");
 
+  const searchArray = donations.filter((donation) => donation.category.toLowerCase() === search)
+  console.log(search);
+  console.log(searchArray);
   return (
     <div>
-      <Banner></Banner>
+      <Banner setSearch={setSearch}></Banner>
+      {/* {typeof(search)} */}
       <div className=" flex justify-center">
         <div className="grid grid-cols-4 gap-7">
           {
-            donations?.map((donation) => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+            !search ? donations?.map((donation) => <DonationCard key={donation.id} donation={donation}></DonationCard>) : searchArray?.map((donation) => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+            // donations?.map((donation) => <DonationCard key={donation.id} donation={donation}></DonationCard>)
           }
         </div>
       </div>
