346b0e192a0b763be3d9d158eb1421ce16169f6c
NF RIDOY <nfridoy@gmail.com>
2023-09-27 20:37:33 +0600

    Feat: show all Btn fuctional

--- src/components/Donate/DonateList.jsx
+++ src/components/Donate/DonateList.jsx
@@ -1,13 +1,14 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import DonateCard from "./DonateCard";
 import { DonationContext } from "../../Layout/MainLayout";
 import DataNotFound from "../../pages/Error/DataNotFound";
 
 export default function DonateList() {
     const [allDonations, setAllDonations] = useContext(DonationContext)
+    const [isShowAll, setIsShowAll] = useState(false)
     useEffect(() => {
         const Donated = JSON.parse(localStorage.getItem("Donated"))
-        if(Donated) {
+        if (Donated) {
             setAllDonations(Donated)
         }
         else {
@@ -19,9 +20,12 @@ export default function DonateList() {
         <div>
             <div className="grid grid-cols-2 gap-8">
                 {
-                    localStorage.getItem("Donated") ? allDonations.map((donate) => <DonateCard key={donate.id} donate={donate}></DonateCard>) : DataNotFound()
+                    localStorage.getItem("Donated") ? isShowAll ? allDonations.slice(0, 4).map((donate) => <DonateCard key={donate.id} donate={donate}></DonateCard>) : allDonations.slice(0, 12).map((donate) => <DonateCard key={donate.id} donate={donate}></DonateCard>) : DataNotFound()
                 }
-                
+
+            </div>
+            <div className="w-full flex justify-center">
+                <button onClick={()=> setIsShowAll(!isShowAll)} className="btn bg-[#009444] text-white mx-auto my-10 hover:bg-black">{isShowAll ? "Show Less" : "Show All"}</button>
             </div>
             {/* <DonateCard></DonateCard> */}
             {/* {
